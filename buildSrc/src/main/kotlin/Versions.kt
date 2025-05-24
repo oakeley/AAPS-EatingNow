91fc4b68419c57ff622ac243f767fb44466095af
Edward <edward@oakeley.com>
2025-05-24 14:44:21 +0200

    Fix a couple of config files manually

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -3,17 +3,18 @@ import org.gradle.api.JavaVersion
 
 object Versions {
 
-    const val appVersion = "3.2.0.4"+"-EatingNow"
+    const val appVersion = "3.3.2.0"+"-EatingNow"
     const val versionCode = 1500
-
+    const val kotlin = "1.9.22"
+    const val jvmTarget = "17"             // Change from "11" to "17"
     const val ndkVersion = "21.1.6352462"
 
     const val compileSdk = 34
     const val minSdk = 28
-    const val targetSdk = 28
+    const val targetSdk = 34
     const val wearMinSdk = 26
     const val wearTargetSdk = 29
 
-    val javaVersion = JavaVersion.VERSION_11
+    val javaVersion = JavaVersion.VERSION_17
     const val jacoco = "0.8.11"
 }
